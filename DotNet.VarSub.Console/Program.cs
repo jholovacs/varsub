f3884426f3b5d14c39f9372df3b621d54d2ea608
Jeremy Holovacs <jeremy.holovacs@erpmaestro.com>
2021-04-27 08:54:08 -0400

    Improvements for command line error control

--- DotNet.VarSub.Console/Program.cs
+++ DotNet.VarSub.Console/Program.cs
@@ -10,7 +10,53 @@ namespace DotNet.VarSub.Console
 {
 	public class Program
 	{
+		private static ILoggerFactory _loggerFactory;
+		private static ILogger _logger;
+
 		public static int Main(string[] args)
+		{
+			try
+			{
+				ConfigureLogging();
+				_logger = _loggerFactory.CreateLogger<Program>();
+
+				var rootCommand = GenerateRootCommand();
+
+				rootCommand.Handler = CommandHandler.Create<FileInfo, string, string>(
+					async (file, parameterName, value) =>
+					{
+						try
+						{
+							await using var readStream = File.OpenRead(file.FullName);
+							var subber = new Subber(_loggerFactory);
+
+							await subber.ReadDocument(readStream);
+							subber.Sub(parameterName, value);
+
+							await using var writeStream = File.OpenWrite(file.FullName);
+							await subber.WriteDocument(writeStream);
+
+							_logger.LogInformation(
+								$"Updated the value for parameter '{parameterName}' in file '{file.FullName}'.");
+							return 0;
+						}
+						catch (Exception ex)
+						{
+							_logger.LogError(ex, "Cannot run the variable substitution. The exception is included.");
+							return -1;
+						}
+					});
+
+				return rootCommand.InvokeAsync(args).Result;
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Failed to execute command line. The exception is included.");
+				return -1;
+			}
+		}
+
+		private static RootCommand GenerateRootCommand()
 		{
 			var rootCommand = new RootCommand
 			{
@@ -21,51 +67,33 @@ namespace DotNet.VarSub.Console
 
 			rootCommand.Description = "variable substitution tool";
 			rootCommand.Name = "varsub";
-
-			rootCommand.Handler = CommandHandler.Create<FileInfo, string, string>(async (file, parameterName, value) =>
-			{
-				var loggingFactory = ConfigureLogging();
-				var logger = loggingFactory.CreateLogger<Program>();
-				try
-				{
-					await using var readStream = File.OpenRead(file.FullName);
-					var subber = new Subber(loggingFactory);
-
-					await subber.ReadDocument(readStream);
-					subber.Sub(parameterName, value);
-
-					await using var writeStream = File.OpenWrite(file.FullName);
-					await subber.WriteDocument(writeStream);
-
-					logger.LogInformation(
-						$"Updated the value for parameter '{parameterName}' in file '{file.FullName}'.");
-					return 0;
-				}
-				catch (Exception ex)
-				{
-					logger.LogError(ex, "Cannot run the variable substitution. The exception is included.");
-					return -1;
-				}
-			});
-
-			return rootCommand.InvokeAsync(args).Result;
+			return rootCommand;
 		}
 
-		private static ILoggerFactory ConfigureLogging()
+		private static void ConfigureLogging()
 		{
-			var loggingFactory = LoggerFactory.Create(config =>
+			try
 			{
-				config.AddSimpleConsole(consoleConfig =>
+				_loggerFactory = LoggerFactory.Create(config =>
 				{
-					consoleConfig.ColorBehavior = LoggerColorBehavior.Enabled;
-					consoleConfig.TimestampFormat = "yyyy-MM-ddThh:mm:ss ";
-					consoleConfig.UseUtcTimestamp = true;
-					consoleConfig.IncludeScopes = true;
-					consoleConfig.SingleLine = false;
+					config.AddSimpleConsole(consoleConfig =>
+					{
+						consoleConfig.ColorBehavior = LoggerColorBehavior.Enabled;
+						consoleConfig.TimestampFormat = "yyyy-MM-ddThh:mm:ss ";
+						consoleConfig.UseUtcTimestamp = true;
+						consoleConfig.IncludeScopes = true;
+						consoleConfig.SingleLine = false;
+					});
+					config.Configure(builder => { });
 				});
-				config.Configure(builder => { });
-			});
-			return loggingFactory;
+			}
+			catch (Exception ex)
+			{
+				//can't log this as the error occurred setting up logging. Just emit to stderr
+				System.Console.Error.WriteLine(
+					"Failed to configure logging mechanism.  The exception is as follows: \r\n{0}", ex);
+				throw;
+			}
 		}
 	}
 }
